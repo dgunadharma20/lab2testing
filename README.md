2ae16ad2aa4b97f3f6717ea5c2a9702e5624fb53
dgunadharma20 <dgunadharma20@gmail.com>
2022-10-10 16:05:44 -0700

    modified files and added images

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# lab2testing
+# lab 2 demo
+
+## welcome to lab 2 
+
+- pasta
+- bread
+- chicken
+
+1. INFO 340
+2. CSE 373
+3. INFO 371
+
+!["photo of info 201"](images/info201img.jpeg)
+
+["lab 2 repository link"](https://github.com/dgunadharma20/lab2testing)
---
+++ testingscript.R
@@ -0,0 +1,20 @@
+# creating variables
+
+my_var <- 10
+
+# functions
+
+# create a function that will return the cubed result of given number x
+cubed <- function(x) {
+  answer <- x^3
+  return(answer)
+}
+
+cubed <- function(x) {
+  x <- 2
+  answer <- x^3
+  print(answer)
+  return(answer)
+}
+
+
